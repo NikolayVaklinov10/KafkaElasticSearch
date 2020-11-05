1df7230cfd6f70db683d0547b44ffe777331c64f
Nick <nikolayvaklinov@yahoo.com>
2020-11-05 22:35:29 +0000

    the test for checking the elastic search class

---
+++ src/test/java/com/kafka/elasticsearch/ElasticSearchConsumerTest.java
@@ -0,0 +1,19 @@
+package com.kafka.elasticsearch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElasticSearchConsumerTest {
+
+    @org.junit.jupiter.api.Test
+    void createClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createConsumer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+}
